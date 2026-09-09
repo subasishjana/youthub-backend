e186df17cadde38db4524105e1a7045e920176ed
subasish jana <subasishjana@users.noreply.github.com>
2026-09-09 16:59:20 +0530

    add subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from "mongoose";
+
+const subsciptionSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    channel: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const subscription = mongoose.model("subscription", subsciptionSchema);
