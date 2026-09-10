69c07e61804e2fa9ac9326cfe5874a34c54b6290
subasish jana <subasishjana@users.noreply.github.com>
2026-09-10 08:42:22 +0530

    aggregation pipelines

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -16,4 +16,4 @@ const subscriptionSchema = new Schema(
   }
 );
 
-export const subscription = mongoose.model("subscription", subscriptionSchema);
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
